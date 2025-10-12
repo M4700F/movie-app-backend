b01d82ebfb95c822f8b2ab95c61f26c27b567d54
Maroof Ahmed <96487710+M4700F@users.noreply.github.com>
2025-10-12 09:24:44 +0600

    Create User entity

---
+++ src/main/java/org/example/movieappbackend/payloads/UserDto.java
@@ -0,0 +1,27 @@
+package org.example.movieappbackend.payloads;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class UserDto {
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Email(message = "Email address is not valid")
+    private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 10, message = "Password must be minimum of 3 characters and maximum of 10 characters long")
+    private String password;
+
+    @NotEmpty
+    private String about;
+}
