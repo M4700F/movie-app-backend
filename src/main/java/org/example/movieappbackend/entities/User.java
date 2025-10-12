f2e3e35193ba8515d5ae62385fd67b0e4ff53687
Maroof Ahmed <96487710+M4700F@users.noreply.github.com>
2025-10-12 11:28:55 +0600

    Finish Security Package

--- src/main/java/org/example/movieappbackend/entities/User.java
+++ src/main/java/org/example/movieappbackend/entities/User.java
@@ -4,13 +4,18 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,4 +27,34 @@ public class User {
     private String password;
 
     private String about;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
--- src/main/java/org/example/movieappbackend/payloads/UserDto.java
+++ src/main/java/org/example/movieappbackend/payloads/UserDto.java
@@ -24,4 +24,7 @@ public class UserDto {
 
     @NotEmpty
     private String about;
+
+
+    // set of roles
 }
