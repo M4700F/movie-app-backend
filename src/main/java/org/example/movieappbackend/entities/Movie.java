a7c764adb33a9ff0e9d377eb204cbe3662d243ee
Maroof Ahmed <maroofahmed@iut-dhaka.edu>
2025-10-13 20:27:37 +0600

    Create Movie, Rating, Recommendation entity

---
+++ src/main/java/org/example/movieappbackend/entities/Movie.java
@@ -0,0 +1,21 @@
+package org.example.movieappbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "movies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private Integer releaseYear;
+    private String genres;
+}
---
+++ src/main/java/org/example/movieappbackend/entities/Rating.java
@@ -0,0 +1,27 @@
+package org.example.movieappbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ratings")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    private double score; // e.g. 1.0–5.0
+}
