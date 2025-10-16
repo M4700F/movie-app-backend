811ce0fbe69bfb5e3fd9efd6eb4b5b02cd77e99c
Maroof Ahmed <maroofahmed@iut-dhaka.edu>
2025-10-16 12:59:36 +0600

    Recommendation Service

---
+++ src/main/java/org/example/movieappbackend/repositories/RatingRepo.java
@@ -0,0 +1,19 @@
+package org.example.movieappbackend.repositories;
+
+import org.example.movieappbackend.entities.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RatingRepo extends JpaRepository<Rating, Long> {
+    List<Rating> findByUserId(Long userId);
+
+    @Query("SELECT COUNT(r) FROM Rating r WHERE r.user.id = :userId")
+    Long countByUserId(Long userId);
+
+    @Query("SELECT AVG(r.score) FROM Rating r WHERE r.user.id = :userId")
+    Double averageScoreByUserId(Long userId);
+}
