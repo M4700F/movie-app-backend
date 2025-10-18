1ed5870d68bb149011d9f39242579f373815a1f5
Faiyaz <faiyaz.abrar.fa@gmail.com>
2025-10-19 02:30:15 +0600

    models, services and entities updated for WatchList Feature

---
+++ src/main/java/org/example/movieappbackend/entities/WatchLater.java
@@ -0,0 +1,36 @@
+package org.example.movieappbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_watch_later")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WatchLater {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "movie_id", nullable = false)
+    private Long movieId;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/org/example/movieappbackend/payloads/FavoriteDto.java
@@ -0,0 +1,19 @@
+package org.example.movieappbackend.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FavoriteDto {
+    private Long id;
+    private Long userId;
+    private Long movieId;
+    private LocalDateTime createdAt;
+}
